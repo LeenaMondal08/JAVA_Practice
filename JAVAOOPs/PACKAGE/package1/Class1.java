f500b036aafc83d42a43a33b35a8e4f77261a6b2
LeenaMondal08 <leenamondal1813@gmail.com>
2025-04-14 09:55:32 +0530

    package

---
+++ JAVAOOPs/PACKAGE/package1/Class1.java
@@ -0,0 +1,6 @@
+package package1;
+public class Class1{
+    public void get(){
+        System.out.println("inside the pacakge1");
+    }
+}
---
+++ JAVAOOPs/PACKAGE/package2/Class2.java
@@ -0,0 +1,6 @@
+package package2;
+public class Class2{
+    public void get1(){
+        System.out.println("inside the package2");
+    }
+}
