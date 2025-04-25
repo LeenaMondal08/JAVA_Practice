acefaae0bd4b70dc03ef0bf7c892be5bde432fb9
LeenaMondal08 <leenamondal1813@gmail.com>
2025-04-25 07:33:40 +0530

    superkeyword

---
+++ STATIC_KEYWORD/staticvariable/staticblock/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    static{
+        System.out.println("this is the static block");
+    }
+    public static void main(String args[]){
+        System.out.println("hello main");
+    }
+    static{
+        System.out.println("this is the another  static block");
+    }
+}
