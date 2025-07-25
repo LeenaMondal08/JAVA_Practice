2521cbd95511ecfe76762654763f564f45acc6d3
LeenaMondal08 <leenamondal1813@gmail.com>
2025-07-25 18:01:15 +0530

    math.sqart

---
+++ gfg_problem/SquareRoot.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+class SquareRoot {
+    public static int floorvalue(int n){
+        return (int)Math.sqrt(n);
+    }
+    public static void main(String[] args) {
+        System.out.println( floorvalue(81));
+    }
+}
