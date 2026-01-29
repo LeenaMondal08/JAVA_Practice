1a394f4bdab3b7a8fc212ca36c1992327a91f5ef
LeenaMondal08 <leenamondal1813@gmail.com>
2026-01-29 09:39:08 +0530

    incrementop

---
+++ variable_datatype/Increment.java
@@ -0,0 +1,9 @@
+ class Increment {
+    public static void main(String args[]){
+        int a=20;
+        int b=(a++)+(a++)+(a++);
+        int c=(a++)+(b++)+(b++);
+        int d=(c++)+(c++)-(a++)-(b++);
+        System.out.println(a+" "+b+" "+c+" "+d);
+    }
+}
